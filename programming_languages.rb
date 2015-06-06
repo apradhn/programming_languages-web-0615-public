7ab5e442742b062b77c6416faea37598ce307757
apradhn <abhijit.pradhan6@gmail.com>
2015-06-06 18:58:41 -0400

    all done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  reformat = {}
+  languages.each do |style_key, style|
+    style.each do |lang_key, lang|
+
+      if reformat[lang_key].nil?
+        reformat[lang_key] = {}
+      end
+      reformat[lang_key][:type] = lang[:type]
+
+      if reformat[lang_key][:style].nil?
+        reformat[lang_key][:style] = [style_key]
+      elsif style_key == :oo
+        reformat[lang_key][:style] << style_key
+      elsif style_key == :functional
+        reformat[lang_key][:style] << style_key 
+      end
+      
+    end
+  end
+
+  reformat
 end
